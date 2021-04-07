d1fe52178bf7fc9d8e2871fdc5c212ffd0fc0969
unknown <adard2002@gmail.com>
2021-04-07 17:35:36 -0500

    moved most of the amenity code to its own file

--- AsyncWeb/Models/Interfaces/IAmenityRepository.cs
+++ AsyncWeb/Models/Interfaces/IAmenityRepository.cs
@@ -1,8 +1,5 @@
-﻿using AsyncWeb.Data;
-using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace AsyncWeb.Models.Interfaces
@@ -10,75 +7,9 @@ namespace AsyncWeb.Models.Interfaces
     public interface IAmenityRepository
     {
 
-        Task<IEnumerable<Amenity>> GetAllAmenities();
-        Task CreateAmenity(Amenity amenity);
-        Task<Amenity> GetAmenity(int id);
-        Task<bool> UpdateAmenity(Amenity amenity);
+        Task<IEnumerable<Amenities>> GetAllAmenities();
+        Task CreateAmenity(Amenities amenities);
+        Task<Amenities> GetAmenity(int id);
+        Task<bool> UpdateAmenity(Amenities amenities);
     }
-
-
-    public class DatabaseAmenityRepository : IAmenityRepository
-    {
-        private readonly SchoolDbContext _context;
-
-        public DatabaseAmenityRepository(SchoolDbContext context)
-        {
-            _context = context;
-        }
-
-        public async Task CreateAmenity(IAmenityRepository amenity)
-        {
-            _context.Amenities.Add(amenity);
-            await _context.SaveChangesAsync();
-        }
-
-        public async Task<IEnumerable<Amenity>> GetAllAmenities()
-        {
-            return await _context.Amenities.ToListAsync();
-        }
-
-        public async Task<Amenity> GetAmenity(int id)
-        {
-            return await _context.Amenities.FindAsync(id);
-        }
-
-        public async Task<bool> UpdateAmenity(IAmenityRepository amenity)
-        {
-            _context.Entry(amenity).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-
-                return true;
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!AmenityExists(amenity.Id))
-                {
-                    return false;
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-        }
-
-        private bool AmenityExists(object id)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-
-
-
-
-
-
-
-
-
 }

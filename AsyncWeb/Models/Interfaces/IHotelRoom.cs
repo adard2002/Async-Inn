fbc9000306a2b14f178e3095f40315d1c90a3cc6
unknown <adard2002@gmail.com>
2021-04-09 17:14:26 -0500

    attempted a bit of the IHotelRoom interface part of lab14

--- AsyncWeb/Models/Interfaces/IHotelRoom.cs
+++ AsyncWeb/Models/Interfaces/IHotelRoom.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 
 namespace AsyncWeb.Models.Interfaces
 {
-    interface IHotelRoom
+    [ServiceContract]
+    public interface IHotelRoom
     {
 
-
+        [OperationContract]
+        HotelRoom Add(double n1, double n2)
 
 
 
